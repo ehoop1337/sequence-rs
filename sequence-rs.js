a597ead96b9bcdcac406f98147c75b4941fc4155
Sayfutdinov Ruslan <79902917+ehoop1337@users.noreply.github.com>
2022-05-18 11:02:36 +0300

    v0.1.0

--- sequence-rs.js
+++ sequence-rs.js
@@ -1,241 +1,371 @@
 class SequenceRS {
-		
-		#requestId = 0;
-		#fps = null;
-		#fpsInterval = null;
-		#startTime = null;
-		#now = null;
-		#then = null;
-		#elapsed = null;
-		
-		constructor(settings) {
-			this.canvas = settings.canvas;
-			this.context = this.canvas.getContext('2d');
-			this.width = settings.width || 1920;
-			this.height = settings.height || 950;
-			this.frames = settings.frames;
-			this.fps = settings.fps || 0;
-			this.path = settings.path || '/';
-			this.webp = settings.webp || false;
-			this.browserIsWebpSupported = this.#isWebpSupported();
-			this.format = settings.format || 'jpg';
-			this.name = settings.name || '';
-			this.pad = settings.pad || 5;
-			this.arrayFrames = new Array();
-			this.loadedFrames = 0;
-			this.currentFrame = settings.startFrame || 0;
-			this.upload = settings.upload || false;
-			this.obsession = settings.obsession || false;
-			this.start = settings.start || false;
-			this.setSizes();
-			if (this.upload) {
-				this.load();
+					
+	constructor (settings) {
+		this.context = null;
+		this.arrayFrames = [];
+		this.animation = false;
+		this.currentFrame = 0;
+		this.requestId = 0;
+		this.fpsInterval = null;
+		this.startTime = null;
+		this.now = null;
+		this.then = null;
+		this.elapsed = null;
+		this.loadedFrames = 0;
+		this.canvas = settings.canvas;
+		this.width = settings.width;
+		this.height = settings.height;
+		this.countFrames = settings.frames.count;
+		this.pathFrames = settings.frames.path;
+		this.nameFrames = settings.frames.name;
+		this.padFrames = settings.frames.pad;
+		this.formatFrames = settings.frames.format;
+		this.widthFrames = settings.frames.width || settings.width;
+		this.heightFrames = settings.frames.height || settings.height;
+		this.webp = settings.frames.webp || false;
+		this.sx = settings.frames.sx || 0;
+		this.sy = settings.frames.sy || 0;
+		this.x = settings.frames.x || 0;
+		this.y = settings.frames.y || 0;
+		this.loop = settings.loop || false;
+		this.fps = settings.fps || 0;
+		this.startFrame = settings.startFrame || 0;
+		this.renderFirstFrame = settings.preview || false;
+		this.startAfterLoading = settings.startAfterLoading || false;
+		this.events = settings.on || {};
+		this.preventDefault = settings.preventDefault || false;
+		this.addListener();
+		if (this.checkRequiredSettings()) {
+			if (!this.preventDefault) {
+				this.init();
 			}
-			this.animation = false;
 		}
-		
-		#isWebpSupported() {
-			return new Promise((resolve,reject) => {
-				const img = new Image();
-				img.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-				img.onload = img.onerror = function () {
-					resolve(img.height === 2 ? true : false);
+	}
+
+	checkRequiredSettings = () => {
+		if (!this.canvas) return this.errorRequired('canvas');
+		if (!this.width) return this.errorRequired('width');
+		if (!this.height) return this.errorRequired('height');
+		if (!this.countFrames) return this.errorRequired('count');
+		if (!this.pathFrames) return this.errorRequired('path');
+		if (!this.nameFrames) return this.errorRequired('name');
+		if (!this.padFrames) return this.errorRequired('pad');
+		if (!this.formatFrames) return this.errorRequired('format');
+		return true;
+	}
+
+	addListener = () => {
+		for (let key in this.events) {
+			this.canvas.addEventListener(key, this.events[key].bind(this));
+		}
+	}
+
+	init = () => {
+		this.context = this.canvas.getContext('2d');
+		this.setSizesCanvas(this.width, this.height);
+		this.setCurrentFrame(this.startFrame);
+		this.eventInit();
+		if (!this.preventDefault) {
+			this.load();
+		}
+	}
+
+	load = () => {
+		this.eventLoad();
+		let loaded = (index, img) => {
+			this.arrayFrames[index] = img;
+			this.loadedOneFrame();
+			this.eventLoading();
+			if (this.checkLoaded()) {
+				this.eventLoaded();
+				if (!this.preventDefault && this.startAfterLoading) {
+					this.start();
 				}
-			});
-		}
-		
-		#getPathFrame = (indexFrame) => {
-			if (this.browserIsWebpSupported && this.webp) {
-				return `${this.path + this.name + indexFrame.toString().padStart(this.pad, "0")}.webp`;
-			} else {
-				return `${this.path + this.name + indexFrame.toString().padStart(this.pad, "0")}.${this.format}`;
+			}
+			if (this.renderFirstFrame) {
+				if (index == this.currentFrame) {
+					this.context.clearRect(0, 0, this.width, this.height);
+					this.context.drawImage(this.arrayFrames[this.currentFrame], 0, 0);
+				}
 			}
 		}
-		
-		getLoadingPercent = () => {
-			return parseInt(this.loadedFrames / (this.frames / 100));
-		}
-		
-		#loadedOneFrame = () => {
-			this.loadedFrames++;
-		}
-		
-		load = () => {
-			for (let i = 0; i < this.frames; i++) {
+		let startLoad = () => {
+			for (let i = 0; i < this.countFrames; i++) {
 				const img = new Image();
-				img.src = this.#getPathFrame(i);
-				img.onload = () => {
-					this.#loadedOneFrame();
-					this.arrayFrames[i] = img;
-					if (i == this.currentFrame) {
-						this.#renderCurrentFrame(i);
-					}
-					this.#checkLoaded();
-				};
+				img.src = this.getPathFrame(i);
+				img.onload = () => loaded(i, img);
 				img.onerror = () => {
 					const reImg = new Image();
-					reImg.src = this.#getPathFrame(i);
-					reImg.onload = () => {
-						this.#loadedOneFrame();
-						this.arrayFrames[i] = reImg;
-						if (i == this.currentFrame) {
-							this.#renderCurrentFrame(i);
-							this.#checkLoaded();
-						}
-					};
+					reImg.src = this.getPathFrame(i);
+					reImg.onload = () => loaded(i, reImg);
 				};
 			}
 		}
-		
-		#checkLoaded = () => {
-			if (this.getLoadingPercent() === 100) {
-				this.#eventLoaded();
-				if (this.start) {
-					if (this.fps > 0) {
-						this.playFPS();
-					} else {
-						this.play();
-					}
-				}
-			}
-		}
-		
-		getCurrentFrame = () => {
-			return this.currentFrame;
-		}
-		
-		setCurrentFrame = (index) => {
-			if (index < this.frames && index >= 0) {
-				this.currentFrame = index;
-			}
-		}
-		
-		setSizes = (width = this.width, height = this.height) => {
-			this.canvas.setAttribute('width', width);
-			this.canvas.setAttribute('height', height);
-		}
-		
-		setFPS = (fps) => {
-			this.fps = fps;
-		}
-		
-		playFPS = () => {
-			this.animation = true;
-			this.#fpsInterval = 1000 / this.fps;
-			this.#then = Date.now();
-			this.#startTime = this.#then;
-			this.#loop();
-		}
-		
-		#loop = () => {
-			if (this.animation === true) {
-				this.play();
-				if (this.fps > 0) {
-					this.#now = Date.now();
-					this.#elapsed = this.#now - this.#then;
-					if (this.#elapsed > this.#fpsInterval) {
-						this.#then = this.#now - (this.#elapsed % this.#fpsInterval);
-						this.#logicAnimation();
-					}
-				} else {
-					this.#logicAnimation();
-				}
-			}
-		}
-		
-		#logicAnimation = () => {
-			if (this.currentFrame < this.frames - 1) {
-				this.currentFrame = this.currentFrame + 1;
-			} else if (this.obsession) {
-				this.currentFrame = 0;
-			} else {
-				this.stop();
-			}
-		}
-		
-		play = () => {
-			this.animation = true;
-			this.#render(() => {
-				this.#requestId = window.requestAnimationFrame(this.#loop);
+		if (this.webp) {
+			this.isWebpSupported().then(res => {
+				startLoad();
 			});
+		} else {
+			startLoad();
 		}
-		
-		stop = () => {
-			if (this.#requestId > 0) {
-			    window.cancelAnimationFrame(this.#requestId);
-			    this.animation = false;
-				this.#requestId = 0;
-				return;
-			}
-		}
-		
-		#render = (callback) => {
-			if (this.animation === true) {
-				this.#eventUpdate();
-				this.context.clearRect(0, 0, this.width, this.height);
-				this.context.drawImage(this.arrayFrames[this.currentFrame], 0, 0);
-				callback();
-			}
-		}
-		
-		#renderCurrentFrame = (index = null) => {
-			this.context.clearRect(0, 0, this.width, this.height);
-			if (index != null) {
-				this.context.drawImage(this.arrayFrames[index], 0, 0);
-			} else {
-				this.context.drawImage(this.arrayFrames[this.currentFrame], 0, 0);
-			}
-		}
-		
-		setObsession = (bool) => {
-			this.obsession = bool;
-		}
-		
-		on = (method, callback) => {
-			this.canvas.addEventListener(method, callback);
-		}
-		
-		#eventUpdate = () => {
-			this.canvas.dispatchEvent(new CustomEvent("update", {
-				bubbles: true,
-				detail: {
-					canvas: this.canvas,
-					context: this.context,
-					width: this.width,
-					height: this.height,
-					frames: this.frames,
-					arrayFrames: this.arrayFrames,
-					currentFrame: this.currentFrame,
-					animation: this.animation
-				}
-			}));
-		}
-		
-		#eventLoaded = () => {
-			this.canvas.dispatchEvent(new CustomEvent("loaded", {
-				bubbles: true,
-				detail: {
-					canvas: this.canvas,
-					context: this.context,
-					width: this.width,
-					height: this.height,
-					frames: this.frames,
-					fps: this.fps,
-					path: this.path,
-					webp: this.webp,
-					browserIsWebpSupported: this.browserIsWebpSupported,
-					format: this.format,
-					name: this.name,
-					pad: this.pad,
-					arrayFrames: this.arrayFrames,
-					loadedFrames: this.loadedFrames,
-					currentFrame: this.currentFrame,
-					upload: this.upload,
-					obsession: this.obsession,
-					start: this.start,
-					animation: this.animation
-				}
-			}));
-		}
-		
 	}
+
+	start = (indexFrame) => {
+		if (this.animation === true) return false;
+		this.eventStart();
+		this.animation = true;
+		this.setCurrentFrame(indexFrame ? indexFrame : this.startFrame);
+		if (this.fps > 0) {
+			this.fpsInterval = 1000 / this.fps;
+			this.then = Date.now();
+			this.startTime = this.then;
+		}
+		this.looping();
+		return true;
+	}
+
+	pause = () => {
+		if (this.animation === false) return false;
+		this.eventPause();
+		this.animation = false;
+		return true;
+	}
+
+	play = () => {
+		if (this.animation === true) return false;
+		this.eventPlay();
+		this.animation = true;
+		this.looping();
+		return true;
+	}
+
+	stop = (indexFrame) => {
+		if (this.animation === false) return false;
+		this.eventStop();
+		window.cancelAnimationFrame(this.requestId);
+		this.animation = false;
+		this.requestId = 0;
+		this.setCurrentFrame(indexFrame ? indexFrame : this.startFrame);
+		return true;
+	}
+
+	restart = (indexFrame) => {
+		this.eventRestart(); // Вызываем событие
+		this.setCurrentFrame(indexFrame ? indexFrame : this.startFrame);
+		if (this.animation !== true) {
+			this.animation = true;
+			this.looping();
+		}
+		return true;
+	}
+
+	render = (callback) => {
+		if (this.animation === true) {
+			this.context.clearRect(0, 0, this.width, this.height);
+			this.context.drawImage(this.arrayFrames[this.currentFrame], this.sx, this.sy, this.widthFrames, this.heightFrames, this.x, this.y, this.width, this.height);
+			callback();
+		}
+	}
+
+	looping = () => {
+		if (this.animation === true) {
+			this.render(() => {
+				this.requestId = window.requestAnimationFrame(this.looping);
+			});
+			if (this.fps > 0) {
+				this.now = Date.now();
+				this.elapsed = this.now - this.then;
+				if (this.elapsed > this.fpsInterval) {
+					this.then = this.now - (this.elapsed % this.fpsInterval);
+					this.animate();
+				}
+			} else {
+				this.animate();
+			}
+		}
+	}
+
+	animate = () => {
+		if (this.currentFrame < this.countFrames - 1) {
+			this.currentFrame = this.currentFrame + 1;
+			this.eventUpdate();
+		} else if (this.loop) {
+			this.currentFrame = 0;
+			this.eventUpdate();
+		} else {
+			this.stop();
+		}
+	}
+
+	setStartFrame = (indexFrame) => {
+		this.startFrame = indexFrame;
+		return true;
+	}
+
+	getStartFrame = () => {
+		return this.startFrame;
+	}
+
+	setLoop = (isLoop) => {
+		this.loop = isLoop;
+		return true;
+	}
+
+	getLoop = () => {
+		return this.loop;
+	}
+
+	setFPS = (num) => {
+		this.fps = num;
+		return true;
+	}
+
+	getFPS = () => {
+		return this.fps;
+	}
+
+	setSizesCanvas = (width, height) => {
+		this.width = width;
+		this.height = height;
+		this.canvas.setAttribute('width', width);
+		this.canvas.setAttribute('height', height);
+		return true;
+	}
+
+	getSizesCanvas = () => {
+		return [this.width, this.height];
+	}
+
+	setSizesFrames = (width, height) => {
+		this.widthFrames = width;
+		this.heightFrames = height;
+		return true;
+	}
+
+	getSizesFrames = () => {
+		return [this.widthFrames, this.heightFrames];
+	}
+
+	setCurrentFrame = (indexFrame) => {
+		this.currentFrame = indexFrame;
+		return true;
+	}
+
+	getCurrentFrame = () => {
+		return this.currentFrame;
+	}
+
+	on = (method, callback) => {
+		this.canvas.addEventListener(method, callback.bind(this));
+	}
+
+	eventInit = () => {
+		this.canvas.dispatchEvent(new CustomEvent("init", {
+			bubbles: true,
+			detail: {}
+		}));
+	}
+
+	eventLoad = () => {
+		this.canvas.dispatchEvent(new CustomEvent("load", {
+			bubbles: true,
+			detail: {}
+		}));
+	}
+
+	eventLoading = () => {
+		this.canvas.dispatchEvent(new CustomEvent("loading", {
+			bubbles: true,
+			detail: {}
+		}));
+	}
+
+	eventLoaded = () => {
+		this.canvas.dispatchEvent(new CustomEvent("loaded", {
+			bubbles: true,
+			detail: {}
+		}));
+	}
+
+	eventStart = () => {
+		this.canvas.dispatchEvent(new CustomEvent("start", {
+			bubbles: true,
+			detail: {}
+		}));
+	}
+
+	eventPause = () => {
+		this.canvas.dispatchEvent(new CustomEvent("pause", {
+			bubbles: true,
+			detail: {}
+		}));
+	}
+
+	eventPlay = () => {
+		this.canvas.dispatchEvent(new CustomEvent("play", {
+			bubbles: true,
+			detail: {}
+		}));
+	}
+
+	eventStop = () => {
+		this.canvas.dispatchEvent(new CustomEvent("stop", {
+			bubbles: true,
+			detail: {}
+		}));
+	}
+
+	eventRestart = () => {
+		this.canvas.dispatchEvent(new CustomEvent("restart", {
+			bubbles: true,
+			detail: {}
+		}));
+	}
+
+	eventUpdate = () => {
+		this.canvas.dispatchEvent(new CustomEvent("update", {
+			bubbles: true,
+			detail: {}
+		}));
+	}
+
+	eventLooped = () => {
+		this.canvas.dispatchEvent(new CustomEvent("looped", {
+			bubbles: true,
+			detail: {}
+		}));
+	}
+
+	errorRequired = (text) => {
+		console.error('Required parameter is not specified: ', text);
+		return false;
+	}
+
+	isWebpSupported = () => {
+		let promise = new Promise( (resolve, reject) => {
+			const img = new Image();
+			img.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+			img.onload = img.onerror = function () {
+				resolve(img.height === 2 ? true : false);
+			}
+		});
+		return promise.then(value => this.webp = value);
+	}
+
+	getPathFrame = (indexFrame) => {
+		return `${this.pathFrames + this.nameFrames + indexFrame.toString().padStart(this.padFrames, '0')}.${ (this.webp === true) ? 'webp' : this.formatFrames}`;
+	}
+
+	checkLoaded = () => {
+		return (this.getLoadingPercent() === 100) ? true : false;
+	}
+
+	getLoadingPercent = () => {
+		return parseInt(this.loadedFrames / (this.countFrames / 100));
+	}
+
+	loadedOneFrame = () => {
+		this.loadedFrames++;
+	}
+
+}
